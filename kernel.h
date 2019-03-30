983389968910011bbdcf7240f5911c4722f17e68
Biswajit Biswas <35150880+biswajitcsecu@users.noreply.github.com>
2019-03-31 02:36:29 +0530

    Paralell computing of Hausdorff distance measure
    The Hausdorff distance is a mathematical construct to measure the "closeness" of two sets of points or a single set to a reference point that are subsets of a metric space. Here we implement the distance using CUDA. This program will return the Hausdorff distance between a set of points with a single given data points. The code that computes the  Hausdorff distance for a very large vector using a parallel approach.

---
+++ kernel.h
@@ -0,0 +1,6 @@
+
+#ifndef KERNEL_H
+#define KERNEL_H
+// Kernel wrapper for computing array of distance values
+void distanceArray(float *out, float *in, float ref, int len);
+#endif
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "kernel.h"
+#include <stdlib.h>
+
+
+#define N 64
+
+
+float scale(int i, int n)
+{
+return ((float)i)/(n - 1);
+}
+
+
+int main(){
+
+const float ref = 0.5f;
+float *in = (float*)calloc(N, sizeof(float));
+float *out = (float*)calloc(N, sizeof(float));
+// Compute scaled input values
+for (int i = 0; i < N; ++i)
+{
+in[i] = scale(i, N);
+}
+distanceArray(out, in, ref, N);
+
+free(in);
+free(out);
+return 0;
+}
+
+
